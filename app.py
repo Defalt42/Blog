cc05a6c056ed54f53c3ec056b994a69b48387bcd
defalt42 <gilbertb4375@gmail.com>
2021-10-05 22:47:22 -0500

    Add posts from database to homepage

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import render_template,url_for
+from flask import render_template, url_for, request, redirect
 
-from models import db,Post,app
+from models import db, Post, Category, app
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    category_py = Category(name='Python')
+    category_photography = Category(name='Photography')
+
+    post_1 = Post(author='Bryan Gilbert', title='Python 101', 
+        content='The first step is to start coding.', 
+        category=category_py)
+    post_2 = Post(author='John Wick', title='Photography Basics', 
+        content='Let\'s start with ISO, aperture, and shutter speed.', 
+        category=category_photography)
+
+    category_py.posts.append(post_1)
+    category_photography.posts.append(post_2)
+
+    db.session.add(category_py)
+    db.session.add(category_photography)
+    db.session.commit()
+
+    posts = Post.query.all()
+
+    return render_template("index.html", posts=posts)
+
+
+@app.route('/new-post', methods=['GET','POST'])
+def add_post():
+    if request.form:
+        post = Post(author=request.form['author'], title=request.form['title'], 
+            category=request.form['category'],
+            content=request.form['content'])
+        
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    return render_template('newpost.html')
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -3,14 +3,27 @@ from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
 
+
 class Post(db.Model):
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(50), nullable=False)
     title = db.Column(db.String(80), nullable=False)
-    category = db.Column(db.String(50))
     content = db.Column(db.Text)
-    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    category = db.relationship('Category', backref=db.backref('posts', lazy=True))
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return self.name
