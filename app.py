35a537c8b176fec1f5cf906b60e2c8f23dd28550
defalt42 <gilbertb4375@gmail.com>
2021-10-10 20:42:38 -0500

    Convert new post form to Bootstrap form

--- app.py
+++ app.py
@@ -5,23 +5,6 @@ from models import db, Post, Category, app
 
 @app.route("/")
 def index():
-    category_py = Category(name='Python')
-    category_photography = Category(name='Photography')
-
-    post_1 = Post(author='Bryan Gilbert', title='Python 101', 
-        content='The first step is to start coding.', 
-        category=category_py)
-    post_2 = Post(author='John Wick', title='Photography Basics', 
-        content='Let\'s start with ISO, aperture, and shutter speed.', 
-        category=category_photography)
-
-    category_py.posts.append(post_1)
-    category_photography.posts.append(post_2)
-
-    db.session.add(category_py)
-    db.session.add(category_photography)
-    db.session.commit()
-
     posts = Post.query.all()
 
     return render_template("index.html", posts=posts)
