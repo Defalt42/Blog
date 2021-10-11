2a457e09db221d110380e1bd1de8d142c57717fd
defalt42 <gilbertb4375@gmail.com>
2021-10-10 21:39:12 -0500

    Create Add Category form

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import render_template, url_for, request, redirect
 
 from models import db, Post, Category, app
-
+from sqlalchemy.exc import IntegrityError
 
 @app.route("/")
 def index():
-    posts = Post.query.all()
+    posts = Post.query.order_by(Post.pub_date.desc()).all()
 
     return render_template("index.html", posts=posts)
 
@@ -13,7 +13,8 @@ def index():
 @app.route('/new-post', methods=['GET','POST'])
 def add_post():
     if request.form:
-        category = Category(name=request.form['category'])
+        category_name = request.form['category']
+        category = Category.query.filter_by(name=category_name).first()
         post = Post(author=request.form['author'], title=request.form['title'], 
             category=category,
             content=request.form['content'])
@@ -22,8 +23,26 @@ def add_post():
         db.session.commit()
 
         return redirect(url_for('index'))
-    return render_template('newpost.html')
 
+    categories = Category.query.all()
+
+    return render_template('newpost.html', categories=categories)
+
+@app.route('/new-category', methods=['GET', 'POST'])
+def add_category():
+    if request.form:
+        category = Category(name=request.form['category'])
+        db.session.add(category)
+        try:
+            db.session.commit()
+            return redirect(url_for('index'))
+        except IntegrityError:
+            db.session.rollback()
+            # error, there already is a user using this bank address or other
+            # constraint failed
+
+        
+    return render_template('newcategory')
 
 if __name__ == '__main__':
     db.create_all()
--- models.py
+++ models.py
@@ -23,7 +23,7 @@ class Post(db.Model):
 class Category(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
 
     def __repr__(self):
         return self.name
