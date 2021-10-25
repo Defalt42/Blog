0b81feb932e40285799a589793947a6883547164
defalt42 <gilbertb4375@gmail.com>
2021-10-25 18:54:04 -0500

    Integrate Boostrap CSS frontend blog template

--- flask_blog/main.py
+++ flask_blog/main.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, url_for, request, redirect
 
 from . import db
-from .models import Post, Category
+from .models import Post
 from sqlalchemy.exc import IntegrityError
 
 
@@ -17,10 +17,7 @@ def index():
 @main.route('/new-post', methods=['GET','POST'])
 def add_post():
     if request.form:
-        category_name = request.form['category']
-        category = Category.query.filter_by(name=category_name).first()
         post = Post(author=request.form['author'], title=request.form['title'], 
-            category=category,
             content=request.form['content'])
         
         db.session.add(post)
@@ -33,22 +30,4 @@ def add_post():
             # error, there already is a user using this bank address or other
             # constraint failed
 
-    categories = Category.query.all()
-
-    return render_template('newpost.html', categories=categories)
-
-@main.route('/new-category', methods=['GET', 'POST'])
-def add_category():
-    if request.form:
-        category = Category(name=request.form['category'])
-        db.session.add(category)
-        try:
-            db.session.commit()
-            return redirect(url_for('main.index'))
-        except IntegrityError:
-            db.session.rollback()
-            # error, there already is a user using this bank address or other
-            # constraint failed
-
-        
-    return render_template('newcategory.html')
+    return render_template('newpost.html')
--- flask_blog/models.py
+++ flask_blog/models.py
@@ -10,17 +10,6 @@ class Post(db.Model):
     title = db.Column(db.String(80), nullable=False)
     content = db.Column(db.Text)
     pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
-    category = db.relationship('Category', backref=db.backref('posts', lazy=True))
-
-class Category(db.Model):
-    __tablename__ = 'categories'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-
-    def __repr__(self):
-        return self.name
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
