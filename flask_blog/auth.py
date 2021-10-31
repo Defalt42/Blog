190f36dd95ace0252f0fbe8ec5f55015258ae141
defalt42 <gilbertb4375@gmail.com>
2021-10-30 19:58:27 -0500

    Set up log in user

--- flask_blog/auth.py
+++ flask_blog/auth.py
@@ -5,13 +5,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from .forms import LoginForm, SignupForm
 from sqlalchemy.exc import IntegrityError
-
+from flask_login import login_user
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+
+    if request.method == 'POST':
+        user = User()
+        login_user(user)
 
     return render_template("login.html", login_form=login_form)
 
